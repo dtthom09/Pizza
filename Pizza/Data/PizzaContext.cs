c4ebbe7a2368bcebb6c1627e3ab3074cb3d29dd2
Dylan Thomas <dthomas@strategicmarketing.com>
2023-01-23 19:11:22 -0500

    added pizza context

---
+++ Pizza/Data/PizzaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza.Data
+{
+    public  class PizzaContext :DbContext //pizza context derives from db context (think of db context as representing a session within the database)
+    {     
+        
+    }
+}
